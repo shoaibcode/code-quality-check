4b26fdadc18c1c83132bb7d3a9be4454e1723303
Tasmiya92 <tasmiya.gs92@gmail.com>
2021-02-22 01:40:06 +0530

    ESLint

---
+++ index.js
@@ -0,0 +1,7 @@
+import { foo } from "./Test"
+
+function sayHello() {
+    return foo
+}
+
+sayHello()
--- touch.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-var s = 2;
-
-
-class HelloWorld extends React.Component {
-    render() {
-        return <div>Hello</div>
-    }
-}
