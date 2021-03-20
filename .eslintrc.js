a014e99a4b279bba4035885cef489997c710f137
Tasmiya92 <tasmiya.gs92@gmail.com>
2021-03-20 16:44:16 +0530

    setup husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,15 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 6,
+    project: ["./tsconfig.json"],
     sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
   },
-  plugins: ["react"],
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  plugins: ["react", "@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
   rules: {
   },
 };
